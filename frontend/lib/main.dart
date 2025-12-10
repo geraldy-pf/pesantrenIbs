25d5e0cba9d73edd383328e3664e9d428c16098e
geraldy-pf <geralmia01@gmail.com>
2025-12-10 15:49:32 +0700

    update main

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,49 +1,20 @@
 import 'package:flutter/material.dart';
+import 'splash.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Pesantren IBS',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        useMaterial3: true,
-      ),
+      theme: ThemeData(useMaterial3: true),
       home: const SplashPage1(),
       debugShowCheckedModeBanner: false,
-    );
-  }
-}
-
-class SplashPage1 extends StatefulWidget {
-  const SplashPage1({Key? key}) : super(key: key);
-
-  @override
-  State<SplashPage1> createState() => _SplashPage1State();
-}
-
-class _SplashPage1State extends State<SplashPage1> {
-  @override
-  void initState() {
-    super.initState();
-    Future.delayed(const Duration(seconds: 2), () {
-      Navigator.of(context).pushReplacementNamed('/splash_page_2');
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Center(
-        child: Image.asset('assets/img/logo.png'),
-      ),
     );
   }
 }
